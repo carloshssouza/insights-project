7d6200e9f32453752954ac999dfcf23a63944b45
lucasblazzi <lucasblazzi@hotmail.com>
2021-11-06 10:57:09 -0300

    Restructure api service

--- client/app.py
+++ client/app.py
@@ -7,8 +7,7 @@ app.config.from_object(os.environ["APP_SETTINGS"])
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-from models.client import Client
-from models.advisor import Advisor
+from model import Client
 
 
 @app.route("/")
@@ -63,57 +62,6 @@ def client_remove(id_):
         client = Client.query.filter_by(id=id_).update(dict(status=0))
         db.session.commit()
         return jsonify(client)
-    except Exception as e:
-        return str(e)
-
-
-@app.route("/advisors")
-def advisor_list():
-    try:
-        advisors = Advisor.query.filter_by(status=1).all()
-        return jsonify([e.serialize() for e in advisors])
-    except Exception as e:
-        return str(e)
-
-
-@app.route("/advisor/<id_>")
-def advisor_get_by_id(id_):
-    try:
-        advisor = Advisor.query.filter_by(id=id_).first()
-        return jsonify(advisor.serialize())
-    except Exception as e:
-        return str(e)
-
-
-@app.route("/advisor/create", methods=["POST"])
-def advisor_create():
-    try:
-        advisor_input = request.get_json()
-        advisor = Advisor(**advisor_input)
-        db.session.add(advisor)
-        db.session.commit()
-        return f"New advisor registered {advisor.id}"
-    except Exception as e:
-        return str(e)
-
-
-@app.route("/advisor/update/<id_>", methods=["POST"])
-def advisor_update(id_):
-    try:
-        advisor_input = request.get_json()
-        advisor = Advisor.query.filter_by(id=id_).update(dict(**advisor_input))
-        db.session.commit()
-        return jsonify(advisor)
-    except Exception as e:
-        return str(e)
-
-
-@app.route("/advisor/delete/<id_>", methods=["GET"])
-def advisor_remove(id_):
-    try:
-        advisor = Advisor.query.filter_by(id=id_).update(dict(status=0))
-        db.session.commit()
-        return jsonify(advisor)
     except Exception as e:
         return str(e)
 
